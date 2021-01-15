9287e079338b7591891e052ff0ec57ff144145ce
sadamdamz <sadamdamzzz@gmail.com>
2021-01-15 23:11:17 +0530

    meetingRoom

---
+++ screens/MeetingRoom.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MeetingRoom extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default MeetingRoom;
